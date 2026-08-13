5d5e4112bbd2d46d2d88b14ee674400cae5c4216
João <joao.v.vitorino@aluno.senai.br>
2026-08-13 11:07:07 -0300

    Alterações aula 13/08

--- src/todolist/ToDoList.java
+++ src/todolist/ToDoList.java
@@ -4,17 +4,85 @@
  */
 package todolist;
 
-/**
- *
- * @author Aluno
- */
-public class ToDoList {
+import java.util.Scanner;
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
-    }
+public class ToDoList {
+public static void main(String[] args) {
+        Scanner teclado = new Scanner(System.in);
+        
+        // Vetor para guardar no máximo 10 tarefas
+        String[] tarefas = new String[10]; 
+        int quantidade = 0;
+        int opcao = 0;
+
+        System.out.println("---Lista de tarefas---");
+
+        while (opcao != 4) {
+            System.out.println("\n1 Adicionar tarefa");
+            System.out.println("2 Excluir tarefas");
+            System.out.println("3 Listar tarefas");
+            System.out.println("4 Sair");
+            System.out.print("Escolha o numero da opcao: ");
+            opcao = teclado.nextInt();
+            teclado.nextLine();
+
+            if (opcao == 1) {
+              
+                if (quantidade == 10) {
+                    System.out.println("A lista de tarefas esta cheia!");
+                } else {
+                    System.out.print("Digite a tarefa: ");
+                    String texto = teclado.nextLine();
+
+                 
+                    for (int i = 0; i < 10; i++) {
+                        if (tarefas[i] == null) {
+                            tarefas[i] = texto;
+                            quantidade = quantidade + 1;
+                            System.out.println("Tarefa adicionada com sucesso!");
+                            break;
+                        }
+                    }
+                }
+            } 
+            else if (opcao == 2) {
+                if (quantidade == 0) {
+                    System.out.println("Nao tem nada para apagar ainda!");
+                } else {
+                    System.out.println("\nQual você quer apagar? (Digite o número)");
+                    for (int i = 0; i < 10; i++) {
+                        if (tarefas[i] != null) {
+                            System.out.println(i + " - " + tarefas[i]);
+                        }
+                    }
+                    System.out.print("Número da tarefa: ");
+                    int posicaoApagar = teclado.nextInt();
+
+                   
+                    if (posicaoApagar >= 0 && posicaoApagar < 10 && tarefas[posicaoApagar] != null) {
+                        tarefas[posicaoApagar] = null; 
+                        quantidade = quantidade - 1;
+                        System.out.println("Tarefa excluída com sucesso!");
+                    } else {
+                        System.out.println("Ih, essa posição aí não vale!");
+                    }
+                }
+            } 
+            else if (opcao == 3) {
+                if (quantidade == 0) {
+                    System.out.println("Lista vazia, sem nada para fazer!");
+                } else {
+                    System.out.println("\nSua Lista Atual:");
+                    for (int i = 0; i < 10; i++) {
+                        if (tarefas[i] != null) {
+                            System.out.println("[" + i + "] " + tarefas[i]);
+                        }
+                    }
+                }
+            } 
+           
+            }
+        }
+        
     
 }
